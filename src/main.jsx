2c310a96cce1c982bd56f5b2ad3711411a20b256
Lijo-Xavier <lijo241999@gmail.com>
2023-09-27 21:59:14 +0530

    Toggleprovider import changed./context/ToggleContext.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ToggleProvider } from './context/toggleContext.jsx'
+import { ToggleProvider } from './context/ToggleContext.jsx'
 import { MovieListProvider } from './context/MovieListContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
