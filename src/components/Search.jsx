c6edf43427836aa6a81ddbf00867eca64831afc6
Lijo-Xavier <lijo241999@gmail.com>
2023-09-27 22:22:30 +0530

    Header import chagned

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './header/header'
+import Header from './header/Header'
 import './search.css'
 import Login from './Login/Login'
 import Footer from './Footer/Footer'
